e4276f219b2142eb63021aea901b9e11cfcc3781
Lucas Pedro Abreu <lucasp.abreu@gmail.com>
2025-05-30 12:33:45 -0300

    Forgotten commit:s: .npmrc for vercel and auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -22,10 +22,14 @@ export const auth = betterAuth({
     enabled: true,
   },
   socialProviders: {
-    // google: {
-    //   clientId: process.env.GOOGLE_CLIENT_ID as string,
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    // },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
   },
   // plugins: [username(), twoFactor(), anonymous(), admin(), organization()],
 });
