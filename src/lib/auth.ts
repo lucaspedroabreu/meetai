83bc76bf873698e1c67165a248d216e2d4786699
Lucas Pedro Abreu <lucasp.abreu@gmail.com>
2025-05-30 18:08:28 -0300

    update auth baseURL

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -18,6 +18,7 @@ export const auth = betterAuth({
       ...schema,
     },
   }),
+  baseURL: process.env.BETTER_AUTH_URL || "https://www.meetai.com.br",
   emailAndPassword: {
     enabled: true,
   },
